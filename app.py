c679dd61f65a9de4115c4a34a036800415652281
Pruthvik-Machhi <pruthvikmachhi7@gmail.com>
2024-06-01 14:02:37 +0530

    r

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ import string
 from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
+
 
 
 with open('model.pkl', 'rb') as model_file:
